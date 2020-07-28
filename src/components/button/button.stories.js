51ac8024ce27577515d9af468c84d1a135d97070
slawoe <info@slawomirernst.de>
2020-07-28 15:56:42 +0200

    Add Leons solution

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -7,6 +7,15 @@ function createButton(buttonText) {
   button.className = "btn";
   const text = document.createTextNode(buttonText);
   button.append(text);
+
+  //   button.addEventListener("click", function handleClick() {
+  //     alert("Button clicked");
+  //   });
+
+  button.addEventListener("click", () => {
+    alert("Button clicked");
+  });
+
   return button;
 }
 
@@ -31,16 +40,16 @@ export const addToCart = () => {
 
   //newer and better way for single addEventListener
 
-  button.addEventListener("click", () => {
-    const answer = confirm(
-      "Are you sure you don't want more coffee, you cheapskate?"
-    );
-    if (answer === true) {
-      alert("Scrooge! Don't you have any friends you can buy coffee for?");
-    } else {
-      alert("Good boy! Add more coffee! Buy it all!");
-    }
-  });
+  //   button.addEventListener("click", () => {
+  //     const answer = confirm(
+  //       "Are you sure you don't want more coffee, you cheapskate?"
+  //     );
+  //     if (answer === true) {
+  //       alert("Scrooge! Don't you have any friends you can buy coffee for?");
+  //     } else {
+  //       alert("Good boy! Add more coffee! Buy it all!");
+  //     }
+  //   });
 
   return button;
 };
