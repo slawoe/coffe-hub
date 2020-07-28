804330b0b9c1f25af1b6c2e159b38f7b70be5dea
slawoe <info@slawomirernst.de>
2020-07-28 18:46:14 +0200

    Add Coffee Selector Buttons to Details page

---
+++ src/components/quantityselector/quantitySelector.js
@@ -0,0 +1,40 @@
+export function createQuantitySelector() {
+  const div = document.createElement("div");
+  div.className = "quantitySelector";
+
+  const quantitySelectorDecreaseButton = document.createElement("button");
+  quantitySelectorDecreaseButton.className = "quantitySelectorDecrease";
+  div.prepend(quantitySelectorDecreaseButton);
+  const decreaseSVG = document.createElement("img");
+  decreaseSVG.src = "../..assets/minus.svg";
+  quantitySelectorDecreaseButton.append(decreaseSVG);
+
+  const quantitySelectorIncreaseButton = document.createElement("button");
+  quantitySelectorIncreaseButton.className = "quantitySelectorIncrease";
+  div.append(quantitySelectorIncreaseButton);
+  const increaseSVG = document.createElement("img");
+  increaseSVG.src = "../..assets/plus.svg";
+  quantitySelectorIncreaseButton.append(increaseSVG);
+
+  const counterDiv = document.createElement("div");
+  counterDiv.innerHTML = "1";
+  quantitySelectorDecreaseButton.after(counterDiv);
+
+  quantitySelectorDecreaseButton.addEventListener("click", () => {
+    const oldResult = Number(counterDiv.innerHTML);
+    if (oldResult === 2) {
+      quantitySelectorDecreaseButton.disabled = true;
+    }
+    counterDiv.innerHTML = oldResult - 1;
+  });
+
+  quantitySelectorIncreaseButton.addEventListener("click", () => {
+    const oldResult = Number(counterDiv.innerHTML);
+    if (oldResult === 1) {
+      quantitySelectorDecreaseButton.disabled = false;
+    }
+    counterDiv.innerHTML = oldResult + 1;
+  });
+
+  return div;
+}
--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -18,14 +18,15 @@
 // };
 
 import { createButton } from "../../components/button/button";
+import { createQuantitySelector } from "../../components/quantitySelector/quantitySelector";
 
 export default { title: "Pages/Details" };
 
 export const basic = () => {
   const main = document.createElement("main");
 
-  // const quantitySelector = createQuantitySelector();
-  // main.append(quantitySelector)
+  const quantitySelector = createQuantitySelector();
+  main.append(quantitySelector);
 
   const button = createButton("Add to cart");
   main.append(button);
