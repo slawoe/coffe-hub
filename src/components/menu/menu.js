12f476e28f07f1c1b30f479453b5c4d7c5d5486f
slawoe <info@slawomirernst.de>
2020-07-30 17:03:40 +0200

    change navbar and list to JS

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -9,53 +9,29 @@ export const createMenue = () => {
     className: "fullWidth coffee-nav",
   });
 
-  const homeButtonLink = createElement("a", {
-    href: "",
-    className: "coffee-nav__button",
-  });
-  const placeLink = createElement("a", {
-    href: "",
-    className: "coffee-nav__button",
-  });
-  const coffeeCupLink = createElement("a", {
-    href: "",
-    className: "coffee-nav__button",
-  });
-  const profileLink = createElement("a", {
-    href: "",
-    className: "coffee-nav__button",
+  const menuButtons = [
+    { name: "Home", imgSrc: homeButtonSRC },
+    { name: "Map", imgSrc: placeSRC },
+    { name: "Coffees", imgSrc: coffeeCupSRC },
+    { name: "Profile", imgSrc: profileSRC },
+  ];
+
+  menuButtons.forEach((button) => {
+    const buttonLink = createElement("a", {
+      href: "",
+      className: "coffee-nav-button",
+    });
+    const buttonImg = createElement("img", {
+      src: button.imgSrc,
+      alt: button.name,
+    });
+    navBar.append(buttonLink);
+    buttonLink.append(buttonImg);
   });
 
-  const homeButton = createElement("img", {
-    src: homeButtonSRC,
-    alt: "Home Button",
-  });
-  const place = createElement("img", {
-    src: placeSRC,
-    alt: "Map",
-  });
-  const coffeeCup = createElement("img", {
-    src: coffeeCupSRC,
-    alt: "Coffee order",
-  });
-  const profile = createElement("img", {
-    src: profileSRC,
-    alt: "Profile",
-  });
   const badge = createElement("span", {
     className: "coffee-nav__badge",
     innerText: 2,
   });
-
-  //Positioning
-  navBar.append(homeButtonLink);
-  navBar.append(placeLink);
-  navBar.append(coffeeCupLink);
-  navBar.append(profileLink);
-  homeButtonLink.append(homeButton);
-  placeLink.append(place);
-  coffeeCupLink.append(coffeeCup);
-  profileLink.append(profile);
-  coffeeCupLink.append(badge);
   return navBar;
 };
